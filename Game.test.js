c964e96e2964beb21e38be6a8610221251562a8d
yaminmhd <mdyamin.drzee@gmail.com>
2018-09-27 12:17:30 +0800

    game is able to handle a game with all ones

--- Game.test.js
+++ Game.test.js
@@ -7,3 +7,11 @@ test('it should be able to manage a gutter game', () => {
   }
   expect(game.calculateScore()).toEqual(0);
 });
+
+test('it should be able to manage a game with all ones ', () => {
+  game = new Game();
+  for(let i = 0; i< 20; i++){
+    game.roll(1);
+  }
+  expect(game.calculateScore()).toEqual(20);
+});
